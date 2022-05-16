e87f8bb7f535dcf1e40b6c808e1e35a2b4ed8406
Karunesh Kumar <59333448+Karunesh-Kumar1998@users.noreply.github.com>
2022-05-16 09:37:43 +0530

    Create github-action_script.sh

---
+++ github-action_script.sh
@@ -0,0 +1,11 @@
+          ls -a
+          echo
+          echo Is Java installed?
+          java -version
+          echo
+          echo Is Git installed?
+          git --version
+          echo What about build tools?
+          mvn --version
+          gradle --version
+          ant -version
